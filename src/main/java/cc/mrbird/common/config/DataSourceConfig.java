4d8761874f625293f8d09e860ca90a79ba22173f
921886007@qq.com <921886007@qq.com>
2022-12-02 00:03:18 +0800

    use @value to replace environment.get

--- src/main/java/cc/mrbird/common/config/DataSourceConfig.java
+++ src/main/java/cc/mrbird/common/config/DataSourceConfig.java
@@ -1,5 +1,4 @@
 package cc.mrbird.common.config;
-
 import cn.hutool.crypto.asymmetric.KeyType;
 import cn.hutool.crypto.asymmetric.RSA;
 import com.alibaba.druid.pool.DruidDataSource;
@@ -7,35 +6,57 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
 import org.springframework.core.env.Environment;
 
-
-@ImportResource(locations = "classpath*:resource.*.xml")
 @Configuration
 public class DataSourceConfig {
-    @Autowired
-    private Environment environment;
-    @Value("${Salt}")
-    String Salt;
+
+    @Value("${PublicKey}")
+    String PublicKey;
+
+    @Value("${spring.datasource.druid.password}")
+    String password;
+
+    @Value("${spring.datasource.druid.url}")
+    String url;
+
+    @Value("${spring.datasource.druid.username}")
+    String username;
+
+    @Value("${spring.datasource.druid.driverClassName}")
+    String driverClassName;
+
+    @Value("${spring.datasource.druid.db-type}")
+    String db_type;
+
+    @Value("${spring.datasource.druid.initial-size}")
+    int initial_size;
+
+    @Value("${spring.datasource.druid.min-idle}")
+    int min_idle;
+
+    @Value("${spring.datasource.druid.max-active}")
+    int max_active;
+
+    @Value("${spring.datasource.druid.max-wait}")
+    int max_wait;
     @Bean(destroyMethod = "close")
     public DruidDataSource dataSource() {
         DruidDataSource dataSource = new DruidDataSource();
-        String passwd = environment.getProperty("spring.datasource.druid.password");
-        dataSource.setPassword(getPasswd(passwd));
-        dataSource.setUrl(environment.getProperty("spring.datasource.druid.url"));
-        dataSource.setUsername(environment.getProperty("spring.datasource.druid.username"));
-        dataSource.setDriverClassName(environment.getProperty("spring.datasource.druid.driverClassName"));
-        dataSource.setDbType(environment.getProperty("spring.datasource.druid.db-type"));
-        dataSource.setInitialSize(environment.getProperty("spring.datasource.druid.initial-size", Integer.class));
-        dataSource.setMinIdle(environment.getProperty("spring.datasource.druid.min-idle", Integer.class));
-        dataSource.setMaxActive(environment.getProperty("spring.datasource.druid.max-active", Integer.class));
-        dataSource.setMaxWait(environment.getProperty("spring.datasource.druid.max-wait", Integer.class));
+        dataSource.setPassword(getPasswd(password));
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setDriverClassName(driverClassName);
+        dataSource.setDbType(db_type);
+        dataSource.setInitialSize(initial_size);
+        dataSource.setMinIdle(min_idle);
+        dataSource.setMaxActive(max_active);
+        dataSource.setMaxWait(max_wait);
         return dataSource;
     }
     //guide from https://hutool.cn/docs/#/crypto/%E9%9D%9E%E5%AF%B9%E7%A7%B0%E5%8A%A0%E5%AF%86-AsymmetricCrypto
     String getPasswd(String passwd) {
-        RSA rsa = new RSA(null,Salt);
+        RSA rsa = new RSA(null,PublicKey);
         String encrypt2 = rsa.decryptStr(passwd, KeyType.PublicKey);
         return encrypt2;
     }
